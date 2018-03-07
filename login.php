88494f5ba87f76b00fa7142775b549dd962f2ee0
Lurkomorie <fomorden@gmail.com>
2018-03-07 20:38:00 +0200

    Added bootstrap, login,register and home pages

---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Free Bootstrap 4 Login/Register Forms By Wired Dots</title>
+        <meta name="description" content="Love Authority." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+        <link rel="stylesheet" href="assets/css/login.css" />
+    </head>
+    <body>
+        <!--hero section-->
+        <section class="hero">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-8 mx-auto">
+                        <div class="card border-none">
+                            <div class="card-body">
+                                <p class="mt-4 text-white lead text-center">
+                                    Sign in to access your Percifali' account
+                                </p>
+                                <div class="mt-4">
+                                    <form>
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" id="email" value="" placeholder="Enter email address">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control" id="password" value="" placeholder="Enter password">
+                                        </div>
+                                        <label class="custom-control custom-checkbox mt-2">
+                                            <input type="checkbox" class="custom-control-input">
+                                            <span class="custom-control-indicator"></span>
+                                            <span class="custom-control-description text-white">Keep me logged in</span>
+                                        </label>
+                                        <button type="submit" class="btn btn-primary float-right">Sign in</button>
+                                    </form>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <p class="text-center">
+                                    Don't have an account yet? <a href="register.html">Sign Up Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </section>
+
+    </body>
+</html>
