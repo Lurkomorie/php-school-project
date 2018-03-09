cb7f468fb1222e285638a9bb577a1eb2220f1232
Lurkomorie <fomorden@gmail.com>
2018-03-09 11:18:23 +0200

    Some changes

--- services.php
+++ services.php
@@ -5,6 +5,7 @@ $request = $_GET["q"];
 $db -> enableExceptions(true);
 
 //prints error
+//gets error message
 function printErr($message){
     print_r(json_encode(
         ['status' => 'error',
@@ -12,6 +13,7 @@ function printErr($message){
 }
 
 //prints result
+//gets result from sql request and error message
 function result($result, $error){
     if($result != false){
     $finalResult = [];
@@ -29,21 +31,37 @@ function result($result, $error){
 }
 
 //catches errors + prints by result function
-function catchErr($stmt){
+//gets sqlite request and boolean if result needs to show
+function catchErr($stmt, $die){
     try{
         $result = $stmt -> execute();
-        result($result, "No result");
+        if($die){
+            result($result, "No result");
+        }
     }
     catch(Exception $e){
         printErr($e->getMessage());
+        return false;
     }
+    return true;
 }
 
 //prints last inserted row from table
+//gets tableName
 $printLastRow = function ($tableName) use ($db) {
+    $lastid = $db->lastInsertRowid();
     if($stmt = $db -> prepare('SELECT * FROM ' . $tableName . ' WHERE id=:p')) {
-        $stmt->bindValue(':p', 5);
-        catchErr($stmt);
+        $stmt->bindValue(':p', $lastid);
+        catchErr($stmt, true);
+    }
+};
+
+$printLastRowWithId = function ($tableName, $id) use ($db) {
+    if($stmt = $db -> prepare('SELECT * FROM ' . $tableName . ' WHERE id=:p')) {
+        $stmt->bindValue(':p', $id);
+        if(catchErr($stmt, true))
+            return true;
+        else return false;
     }
 };
 
@@ -54,7 +72,7 @@ switch ($request) {
         $param = "%{$_GET["p"]}%";
         if($stmt = $db -> prepare('SELECT * FROM Interest WHERE description LIKE :p')) {
             $stmt->bindValue(':p', $param);
-            catchErr($stmt);
+            catchErr($stmt, true);
         }
         else{
             printErr("Error");
@@ -66,19 +84,26 @@ switch ($request) {
         $param = "{$_GET["p"]}";
         if($stmt = $db -> prepare('INSERT INTO Interest VALUES(NULL,:p)')){
             $stmt->bindValue(':p', $param);
-            catchErr($stmt);
-            $printLastRow('Interest');
+            if(catchErr($stmt, false)){
+                $printLastRow('Interest');
+            }
         }
         else{
-            $errormsg['message']='Error1';
+            $errormsg['message']='Error';
             print_r(json_encode($errormsg));
         }
     break;
 
     case "updateI":
-        if($stmt = $db -> prepare('UPDATE Interest SET description="' . $_GET["p"] . '" WHERE id=' . $_GET["id"])){
-            $resultQ = @$stmt -> execute();
-            @result($resultQ, "Empty");
+    //works
+        $param = "{$_GET["p"]}";
+        $id = "{$_GET["id"]}";
+        if($stmt = $db -> prepare('UPDATE Interest SET description=:p WHERE id=:id')){
+            $stmt->bindValue(':p', $param);
+            $stmt->bindValue(':id', $id);
+            if(catchErr($stmt, false)){
+                $printLastRowWithId('Interest', $id);
+            }
         }
         else{
             $errormsg['message']='Error';
@@ -86,47 +111,43 @@ switch ($request) {
         }
     break;   
     
+    //works
     case "deleteI":
-        $result = $db -> query('DELETE FROM Interest WHERE id=' . $_GET["p"]);
-        if($result){
-            print_r("TRUE");
-            return true;
+        $param = "{$_GET["p"]}";
+        if($stmt = $db -> prepare('DELETE FROM Interest WHERE id=:p')){
+            $stmt->bindValue(':p', $param);
+            if($printLastRowWithId('Interest', $param))
+                {
+                    catchErr($stmt, false);
+                }
         }
-        else {
-            $errormsg['message']="Error Deleting";
+        else{
+            $errormsg['message']='Error';
             print_r(json_encode($errormsg));
         }
     break;
 
+    //works
     case "viewI":   
-        $results = $db -> query('SELECT * FROM Interest WHERE id="'. $_GET["p"].'"');
-        $finalResult = [];
-        while ($row = $results -> fetchArray(SQLITE3_ASSOC)) {
-            $finalResult[] = json_encode($row,JSON_UNESCAPED_UNICODE);
+        $param = "{$_GET["p"]}";
+        if($stmt = $db -> prepare('SELECT * FROM Interest WHERE id=:p')) {
+               $stmt->bindValue(':p', $param);
+               catchErr($stmt, true);
         }
-        if (!empty($finalResult)) {
-            foreach($finalResult as $row) {
-                print_r($row);
-            }
-        } else {
-            $errormsg['message']="Empty";
-            print_r(json_encode($errormsg));
+        else{
+          printErr("Error");
         }
     break;
 
-    case "viewSingleU": 
-        $results = $db -> query('SELECT * FROM Person WHERE id="'. $_GET["p"].'"');
-        $finalResult = [];
-        while ($row = $results -> fetchArray(SQLITE3_ASSOC)) {
-            $finalResult[] = json_encode($row,JSON_UNESCAPED_UNICODE);
+
+    case "viewSingleP": 
+        $param = "{$_GET["p"]}";
+        if($stmt = $db -> prepare('SELECT * FROM Person WHERE id=:p')) {
+            $stmt->bindValue(':p', $param);
+            catchErr($stmt, true);
         }
-        if (!empty($finalResult)) {
-            foreach($finalResult as $row) {
-                print_r($row);
-            }
-        } else {
-            $errormsg['message']="Empty";
-            print_r(json_encode($errormsg));
+        else{
+            printErr("Error");
         }
     break;
     
